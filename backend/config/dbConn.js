6e7a0ea319bfd26dc02dbb7ece27a502cb89a473
idkwattuput <ikhwank322@gmail.com>
2024-01-04 04:32:52 +0800

    updated again

--- backend/config/dbConn.js
+++ backend/config/dbConn.js
@@ -3,7 +3,7 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
     user: "kypjcafe_user",
     password: "U0oz7l0SobhipqCBTqiAAVrilQkBF3IU",
-    host: "dpg-cmar7a6n7f5s7396d3qg-a",
+    host: "dpg-cmar7a6n7f5s7396d3qg-a.singapore-postgres.render.com",
     port: 5432,
     database: "kypjcafe"
 });
