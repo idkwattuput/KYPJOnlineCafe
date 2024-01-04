a8368cf202dd2819dd8299bb3f103e28f3f61229
idkwattuput <ikhwank322@gmail.com>
2024-01-04 17:10:19 +0800

    cors seller

--- backend/config/allowedOrigin.js
+++ backend/config/allowedOrigin.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'https://www.saja.com',
-    'https://kypjonlinecafe.onrender.com/'
+    'https://kypjonlinecafe.onrender.com/',
+    'https://kypjseller.onrender.com/'
 ]
 
 module.exports = allowedOrigins
