a08478753c9f9a4650ae99673b35f4ab4182cfc6
idkwattuput <ikhwank322@gmail.com>
2024-01-04 12:11:56 +0800

    internal conn

--- backend/config/dbConn.js
+++ backend/config/dbConn.js
@@ -1,11 +1,7 @@
-const Pool = require('pg').Pool;
+const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: "kypjcafe_user",
-    password: "U0oz7l0SobhipqCBTqiAAVrilQkBF3IU",
-    host: "dpg-cmar7a6n7f5s7396d3qg-a.singapore-postgres.render.com",
-    port: 5432,
-    database: "kypjcafe"
+    connectionString: process.env.DB_CONN,
 });
 
 module.exports = pool;
