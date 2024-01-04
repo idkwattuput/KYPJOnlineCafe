4bb313e0706259b9125d6c27a21ea2c11d062de6
idkwattuput <ikhwank322@gmail.com>
2024-01-04 14:49:05 +0800

    delete useless link

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -7,7 +7,6 @@ const Footer = () => {
     <footer className='flex justify-center items-center gap-2 p-4'>
         <p className='font-medium'>&copy;2023 Develop By DDWD(S2). All Right Reseved</p>
         <Link to="https://github.com/yaayad/kypjcafe2"><AiFillGithub size={28}/></Link>
-        <Link to="https://www.instagram.com/k_ikhwannnn/?next=%2F"><AiFillInstagram size={28}/></Link>
     </footer>
   )
 }
